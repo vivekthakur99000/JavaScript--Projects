58f4e04d30e8aff4df4cbe29a1fddff50fd2bf3c
vivek thakur <vivekthakur99000@gmail.com>
2023-09-09 22:33:47 +0530

    adding  age calculator app

---
+++ Age Calculator/script.js
@@ -0,0 +1,55 @@
+let dob = document.getElementById('dob');
+dob.max = new Date().toISOString.split("T")[0];
+
+function calculateAge (){
+    let birthDate = new Date(dob.value);
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth()+1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth()+1;
+    let y2 = today.getFullYear();
+
+
+    let d3,m3,y3;
+
+    y3 = y2-y1;
+
+    if(m2 >=  m1){
+        m3 = m2 -m1;
+    }else{
+        y3--;
+        m3 = 12 + m2 -m1;
+    }
+
+
+    if(d2 >= d1){
+        d3 = d2-d1;
+    }else{
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
+    }
+
+    // console.log(d3, m3, y3);
+    document.getElementById("year").innerHTML  = y3;
+    document.getElementById("month").innerHTML  = m3;
+    document.getElementById("day").innerHTML  = d3;
+}
+
+
+function getDaysInMonth(year, month){
+    return new Date(year, month, 0).getDate();
+}
+
+
+
+
+
